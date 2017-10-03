d483376531498ff116ccb23cbda8fdee655fc21f
Dinesh kumar Sharma <dinesh829269@gmail.com>
2017-10-03 22:45:54 +0530

    Schools Admin Form Updates

--- schools/admin.py
+++ schools/admin.py
@@ -42,8 +42,25 @@ class  SchoolAdmin(admin.ModelAdmin):
             	'cooking_vessels_to_be_repaired', 'no_of_serving_vessels', 'no_of_serving_vessels_required', 'mats_available', 'no_of_mats', 'no_of_mats_required', 'mops_available', 'no_of_mops', 'no_of_mops_required',
             	 'broom_available', 'no_of_broom', 'no_of_broom_required', 'dustbin_available', 'no_of_dustbin', 'no_of_dustbin_required',
             	'plant_pots_available', 'plant_pots', 'plant_pots_required', 'green_cover', 'green_cover_required', 'cleanliness', 'cleanliness_description',
-            	 'pest_control_done_in_last_one_year', 'pest_control_required', 'drinking_water', 'drinking_water_filter', 'no_of_drinking_water_filter', 'drinking_water_filter_required'),
-        })
+            	 'pest_control_done_in_last_one_year', 'pest_control_required', 'drinking_water', 'drinking_water_filter', 'no_of_drinking_water_filter', 'drinking_water_filter_required',
+            	 'water_taps_in_kitchen', 'no_of_water_taps_in_kitchen', 'water_taps_in_kitchen_required', 'shelter_in_toilets', 'water_taps_in_toilets', 'no_of_water_taps_required_in_toilets', 'water_storage_containers_in_kitchen', 'no_of_water_storage_containers_required_in_kitchen'
+            	 , 'water_storage_containers_in_kitchen_required', 'water_storage_containers_in_toilets', 'no_of_water_storage_containers_required_in_toilets', 'toilet_available', 
+            	 'toilet_functioning', 'toilet_boys', 'toilet_girls', 'toilet_doors_condition'),
+        }),
+        ('Learning Environment', {
+            'classes': ('collapse',),
+            'fields': ('mural_art_available', 'mural_art_required',
+            	'learning_and_playing_materials_available', 'learning_and_playing_materials_required',
+            	'charts_available', 'charts_required', 'story_books_available', 'story_books_required', 
+            	'drawing_and_art_materials_available', 'drawing_and_art_materials_required', 'library_kits_available', 'library_kits_required', 
+            	'sports_material_available', 'sports_material_required', 'others_requirements', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''),
+        }),
+        ('Mother Engagement', {
+            'classes': ('collapse',),
+            'fields': ('mothers_committee_formed', 'no_of_meetings_conducted_in_last_three_months', 
+            	'meetings_documented_in_register', 'mothers_committee_feedback', 'bal_vikas_samiti_formed', 'bal_vikas_samiti_feedback', 'meetings_documented',
+            	 'arrangements_for_the_children_with_specialneeds_feedback', 'arrangements_for_the_children_with_specialneeds_requirements'),
+        }),
     )
 		
 
