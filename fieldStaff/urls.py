0b9190db3545ab2b9c9f7ee90dbbc32df738306c
nidhi <nidhi@spliceglobal.com>
2017-10-14 20:30:32 +0530

    profile and school form

--- fieldStaff/urls.py
+++ fieldStaff/urls.py
@@ -7,16 +7,17 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.views.decorators.csrf import csrf_exempt
 
-from fieldStaff.views import SchoolListView, SchoolUpdateView, SchoolDetailView
+from fieldStaff.views import SchoolListView, SchoolDetailView, ProfileUpdate, SchoolCreateView
 
 urlpatterns = [
     url(r'^$', SchoolListView.as_view(), name='school_list'),
+    url(r'^add_school$', SchoolCreateView.as_view(), name='add_school'),
     url(r'^school_detail/(?P<pk>\d+)/$', SchoolDetailView.as_view(), name='school-detail'),
     # url(r'^school_update/(?P<pk>\d+)/$', SchoolUpdateView.as_view(), name='school_update'),
 
     # url(r'^appointment_detail/(?P<id>[0-9]+)/$', AppointmentFollowupListView.as_view(), name='appointment_detail'),
     # url(r'^dashboard/$', appointment_list.as_view(), name='dashboard'),
-    # url(r'^profile/$', appointment_list.as_view(), name='profile'),
+    url(r'^profile/$', ProfileUpdate.as_view(), name='profile'),
     url(r'^accounts/logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
 
 
--- fieldStaff/views.py
+++ fieldStaff/views.py
@@ -1,17 +1,51 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render
 
 # Create your views here.
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView
 from django.views.generic import DetailView
 from django.views.generic import ListView
 from django.views.generic import UpdateView
 # from django.views.generic.edit import UpdateView
+from blcindia import settings
+from schools.models import school, Profile, Type
 
-from schools.models import school
 
+@method_decorator(login_required, name='dispatch')
+class ProfileUpdate(UpdateView):
+    model = Profile
+    fields = '__all__'
+    success_url = reverse_lazy('profile')
+    template_name = 'fieldstaff/profile.html'
 
+    def get_object(self):
+        profile, created = Profile.objects.get_or_create(user=self.request.user)
+        return profile
+
+    def form_valid(self, form):
+        self.request.user.first_name = self.request.POST.get('first_name')
+        self.request.user.last_name = self.request.POST.get('last_name')
+        self.request.user.save()
+        return super(ProfileUpdate, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        ctx = super(ProfileUpdate, self).get_context_data(**kwargs)
+        try:
+            user_data = User.objects.filter(username=self.request.user).first()
+            ctx['user'] = User.objects.filter(username=self.request.user).first()
+            ctx['profile'] = Profile.objects.filter(user=self.request.user).first()
+            ctx['domain'] = settings.DOMAIN
+        except Exception as e:
+            pass
+        return ctx
+
+@method_decorator(login_required, name='dispatch')
 class SchoolListView(ListView):
     model = school
     template_name = 'fieldstaff/school_list.html'
@@ -20,7 +54,37 @@ class SchoolListView(ListView):
         context = super(SchoolListView, self).get_context_data(**kwargs)
         return context
 
+@method_decorator(login_required, name='dispatch')
+class SchoolCreateView(CreateView):
+    model = school
+    fields = '__all__'
+    success_url = reverse_lazy('school_list')
+    template_name = 'fieldstaff/add_school.html'
 
+
+    def form_valid(self, form):
+        self.request.user.first_name = self.request.POST.get('first_name')
+        self.request.user.last_name = self.request.POST.get('last_name')
+        self.request.user.save()
+        return super(SchoolCreateView, self).form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        print 'function called  '
+        ctx = super(SchoolCreateView, self).get_context_data(**kwargs)
+        try:
+            user_data = User.objects.filter(username=self.request.user).first()
+            ctx['user'] = User.objects.filter(username=self.request.user).first()
+            ctx['profile'] = Profile.objects.filter(user=self.request.user).first()
+            ctx['school_type'] = Type.objects.all()
+            ctx['domain'] = settings.DOMAIN
+            print ctx
+        except Exception as e:
+            pass
+        return ctx
+
+
+
+@method_decorator(login_required, name='dispatch')
 class SchoolDetailView(DetailView):
     model = school
     template_name = 'fieldstaff/school_detail.html'
@@ -32,5 +96,3 @@ class SchoolDetailView(DetailView):
         context['school'] = school.objects.all()
         return context
 
-class SchoolUpdateView(UpdateView):
-    model = school
